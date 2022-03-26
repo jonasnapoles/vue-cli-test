ba89c7d00d514af612d73e70fe7fc0067b34a4fe
jonasnapoles <jonasnapoles@gitgub.com>
2022-03-26 13:24:54 +0000

    update small project with componets and viex,includinf concepts such as mapMutations, mapState, ForEach others

---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-for="(item, index) of orderedArry" :key="item.id" @click="increase(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{index}} - {{item.name}}
+                <span class="badge bg-primary badge-pill">{{item.amount}}</span>
+            </li>
+        </ul>
+        <button class="btn btn-danger btn-block" @click="reboot" >Reboot</button>
+    </div>
+</template>
+
+<script>
+
+import {mapState, mapMutations} from 'vuex';
+
+export default {
+    name: 'List',
+    computed:{
+        ...mapState(['fruits']),
+        orderedArry(){
+             /* This arranges the objects in descending order */
+            return this.fruits.sort((a, b) => b.amount - a.amount )
+        }
+    },
+    methods: {
+        ...mapMutations(['increase', 'reboot'])
+    },
+
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    fruits: [
+      {name: 'Apple', amount: 0},
+      {name: 'Banana', amount: 0},
+      {name: 'Pineapple', amount: 0},
+      {name: 'Watermelon', amount:0},
+    ]
   },
   mutations: {
+    increase(state, index){
+      state.fruits[index].amount ++
+    },
+    reboot(state){
+      state.fruits.forEach(element => {
+          element.amount = 0
+      })
+ }
   },
   actions: {
   },
